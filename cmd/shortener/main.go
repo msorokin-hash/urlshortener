377c835f3897ee869b78334136d1381fd8231618
Michael Sorokin <michael.sorokine@yandex.ru>
2025-02-24 10:07:54 +0300

    iter1: add simple routes to project

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/msorokin-hash/urlshortener/internal/app"
+)
+
+func main() {
+	port := ":8080"
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", app.AddUrlHandler)
+	mux.HandleFunc("/{hash}", app.GetUrlHandler)
+
+	log.Fatal(http.ListenAndServe(port, mux))
+}
---
+++ internal/app/handler.go
@@ -0,0 +1,61 @@
+package app
+
+import (
+	"io"
+	"net/http"
+	"strings"
+)
+
+func GetUrlHandler(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.Path, "/")
+	if len(parts) < 2 {
+		http.Error(w, "invalid request", http.StatusBadRequest)
+		return
+	}
+
+	dbConn, err := InitDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer dbConn.Close()
+
+	res, err := GetUrlByHash(dbConn, parts[1])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+
+	w.Header().Set("Location", res)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func AddUrlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "invalid request method", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	dbConn, err := InitDB()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer dbConn.Close()
+
+	hashed := HashStringData(string(body))
+	err = CreateUrl(dbConn, string(hashed), string(body))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("http://localhost:8080/" + hashed))
+}
